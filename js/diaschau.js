e1106efb961e53378359623b94f92650724c31fe
Tobias Wolf <towolf@gmail.com>
2016-05-01 18:51:43 +0200

    Update diaschau.js

--- js/diaschau.js
+++ js/diaschau.js
@@ -1,1 +1,15 @@
-titelbild = document.getElementById('titelbild')
+function makeDiaschau() {
+    var bild_nr = 1;
+    return function() {
+        if (bild_nr < 20) {
+            bild_nr++;
+        } else {
+            bild_nr = 1;
+        }
+        var titelbild = document.getElementById("titelbild");
+        titelbild.src = "/img/det/det" + bild_nr + ".jpg"
+    };
+}
+
+var naechstes_bild = makeDiaschau();
+dia_intervall = window.setInterval(naechstes_bild, 3000)
